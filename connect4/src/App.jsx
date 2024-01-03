7531819f2a96bf3db4adcaf42dec0e4c763a2b53
imlxtif <abdelatifguendouz72@gmail.com>
2024-01-03 15:22:54 +0100

    Refreshed .gitignore

---
+++ connect4/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import './fonts/fonts.css';
+import './App.css';
+import Home from './components/Home'
+import Game from './components/Game'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/human" element={<Game />}/>
+          <Route path="/bots" element={<Game />}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ flask_app/routes.py
@@ -0,0 +1,117 @@
+from __init__ import socketio
+from flask import Flask, render_template , request
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+from game_logic import *
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@socketio.on('play_event')
+def play():
+    request_data = request.json
+    
+    if not request_data:
+        emit('response', {"error": "No data provided"}, 400)
+        return
+
+    board = request_data.get("board")
+    turn = request_data.get("turn")
+    mode = request_data.get("mode")
+    play_col = request_data.get("play_col")
+    
+    if board is None or turn is None or mode is None or play_col is None:
+        emit('response', {"error": "Invalid data format"}, 400)
+        return
+    
+    state = ConnectFourBoard(board)
+    err = 0
+    GameState = 0
+    if mode == 1:
+        if turn == 1:
+            play_row , play_col = Play.computerTurn1(state)
+        elif turn == -1:
+            play_row , err = Play.humanTurn2(state, play_col)
+    else:
+        if turn == 1:
+            play_row , play_col = Play.computerTurn1(state)
+        elif turn == -1:
+            play_row , play_col = Play.computerTurn2(state)
+    
+    state.makeMove(play_row , play_col, turn)
+    GameState = getGameState(state, turn)
+
+    emit('response', {"board": state.board, "GameState": GameState, "Err": err})
+    
+@app.route('/timeout', methods=['POST'])
+def timeout():
+    request_data = request.json
+    
+    if not request_data:
+        return {"error": "No data provided"}, 400
+
+    board = request_data.get("board")
+    turn = request_data.get("turn")
+    
+    if board is None or turn is None:
+        return {"error": "Invalid data format"}, 400
+    state = ConnectFourBoard(board)
+    GameState = 0
+    play_row , play_col = Play.playrandom(state)
+
+    state.makeMove(play_row,play_col,turn)
+    GameState = getGameState(state,turn)
+
+    return {"board": state.board,"GameState":GameState,"Err":0}
+
+@socketio.on('timeout_event')
+def timeout():
+    request_data = request.json
+
+    if not request_data:
+        emit('response', {"error": "No data provided"}, 400)
+        return
+
+    board = request_data.get("board")
+    turn = request_data.get("turn")
+
+    if board is None or turn is None:
+        emit('response', {"error": "Invalid data format"}, 400)
+        return
+
+    state = ConnectFourBoard(board)
+    GameState = 0
+    play_row, play_col = Play.playrandom(state)
+
+    state.makeMove(play_row, play_col, turn)
+    GameState = getGameState(state, turn)
+
+    emit('response', {"board": state.board, "GameState": GameState, "Err": 0})
+
+def getGameState(state,turn):
+    if state.gameOver(turn):
+        if state.Tie():
+            return 2
+        else :
+            return turn
+        
+@socketio.on('message')
+def handle_message(msg):
+    print('Message:', msg)
+    socketio.emit('message', msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
