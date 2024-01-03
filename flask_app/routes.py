3949c5a0f5b08c4ac54e1dc4e6afd662380a257c
ISmaillll <ismailbelk2003@gmail.com>
2024-01-03 15:34:40 +0100

    gg

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,24 +1,49 @@
-from __init__ import socketio
 from flask import Flask, render_template , request
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
 from game_logic import *
-
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+CORS(app)
+sio = socketio.server(cors_allowed_origins='*')
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/play', methods=['POST'])
+def play():
+    request_data = request.json
+    
+    if not request_data:
+        return {"error": "No data provided"}, 400
+
+    board = request_data.get("board")
+    turn = request_data.get("turn")
+    mode = request_data.get("mode")
+    play_col = request_data.get("play_col")
+    
+    if board is None or turn is None or mode is None or play_col is None:
+        return {"error": "Invalid data format"}, 400
+    state = ConnectFourBoard(board)
+    err = 0
+    GameState = 0
+    if mode == 1:
+        if turn == 1:
+            play_row , play_col = Play.computerTurn1(state)
+        if turn == -1:
+            play_row , err = Play.humanTurn2(state,play_col)
+    else :
+        if turn == 1:
+            play_row , play_col = Play.computerTurn1(state)
+        if turn == -1:
+            play_row , play_col = Play.computerTurn2(state)
+    state.makeMove(play_row , play_col,turn)
+    GameState = getGameState(state,turn)
+
+    return {"board": state.board,"GameState":GameState,"Err":err}
 
 @socketio.on('play_event')
 def play():
@@ -114,4 +139,4 @@ def handle_message(msg):
     socketio.emit('message', msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
